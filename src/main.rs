399b3841468184633b92e2c413e01b7dccb8146b
evkyeste <evkyeste@outlook.com>
2025-02-16 13:59:41 +0300

    Update a HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut colors = HashMap::new();
-    colors.insert("Red", 10);
-    colors.insert("Blue", 20);
+    let mut scores = HashMap::new();
+    scores.insert("Score", 100);
 
-    for (color, value) in colors {
-        println!("Color: {} - {}", color, value);
-    }
+    // if let Some(score) = scores.get_mut("Score") {
+    //     *score += 50;
+    //     println!("Score: {}", score);
+    // } else {
+    //     println!("Score not found");
+    // }
+
+    scores.entry("Score").and_modify(|e| *e += 50);
+    println!("Score: {:?}", scores);
 }
